88b032aa06dd6bd3839233b4580bd7ad013129c5
Andrei Lapets <a@lapets.io>
2021-12-16 00:54:07 -0500

    Add section on contributions/versioning to README; update package file/version.

--- README.rst
+++ README.rst
@@ -14,7 +14,7 @@ The library allows users to work with congruence classes (or, equivalently, fini
 
 Package Installation and Usage
 ------------------------------
-The package is available on PyPI::
+The package is available on `PyPI <https://pypi.org/project/modulo/>`_::
 
     python -m pip install modulo
 
@@ -37,3 +37,11 @@ Style conventions are enforced using `Pylint <https://www.pylint.org/>`_::
 
     python -m pip install pylint
     pylint modulo
+
+Contributions
+-------------
+In order to contribute to the source code, open an issue or submit a pull request on the `GitHub page <https://github.com/lapets/modulo>`_ for this library.
+
+Versioning
+----------
+Beginning with version 0.2.0, the version number format for this library and the changes to the library associated with version number increments conform with `Semantic Versioning 2.0.0 <https://semver.org/#semantic-versioning-200>`_.
--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name             = 'modulo',
-    version          = '0.1.0.0',
-    packages         = ['modulo',],
-    install_requires = ['egcd',],
-    license          = 'MIT',
-    url              = 'https://github.com/lapets/modulo',
-    author           = 'Andrei Lapets',
-    author_email     = 'a@lapets.io',
-    description      = 'Pure Python library for working with modular arithmetic, congruence classes, and finite fields.',
-    long_description = open('README.rst').read(),
-    test_suite       = 'nose.collector',
-    tests_require    = ['nose'],
+    name="modulo",
+    version="0.2.0",
+    packages=["modulo",],
+    install_requires=["egcd~=0.2",],
+    license="MIT",
+    url="https://github.com/lapets/modulo",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Pure Python library for working with modular arithmetic, " +\
+                "congruence classes, and finite fields.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
 )
